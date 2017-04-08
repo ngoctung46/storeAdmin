01eecccbef0940a6cde0a7e8afb6b058a3a3cf28
ngoctung46 <ngoctung46@gmail.com>
2017-04-08 19:35:31 +0700

    test

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,23 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-
+import { AngularFire, FirebaseListObservable } from 'angularfire2';
+import { LocalNotifications } from '@ionic-native/local-notifications';
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  modifiers: FirebaseListObservable<any>;
+  constructor(public navCtrl: NavController, public localNotify: LocalNotifications) {
+    
+  }
 
-  constructor(public navCtrl: NavController) {
-
+  test(){
+    this.localNotify.schedule({
+      id: 1,
+      title: "test",
+      text: 'test'
+    });
   }
 
 }
